13e970dbb4d4fc2e97116e7b08359ba60335ccc1
Julian Jark <juliuxu@gmail.com>
2022-04-15 14:50:30 +0200

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { MetaFunction } from "@remix-run/node";
+import { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -13,6 +13,23 @@ export const meta: MetaFunction = () => ({
   title: "Julian Jark",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export const links: LinksFunction = () => [
+  { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "32x32",
+    href: "/favicon-32x32.png",
+  },
+  {
+    rel: "icon",
+    type: "image/png",
+    sizes: "16x16",
+    href: "/favicon-16x16.png",
+  },
+  { rel: "manifest", href: "/site.webmanifest" },
+];
 
 export default function App() {
   return (
