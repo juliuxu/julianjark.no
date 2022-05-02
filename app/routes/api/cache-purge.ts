d465ac313cb1295fcc1d8575770aa54d15388cf6
Julian Jark <juliuxu@gmail.com>
2022-05-02 23:23:44 +0200

    make cache-purge endpoint stream progress

--- app/routes/api/cache-purge.ts
+++ app/routes/api/cache-purge.ts
@@ -1,48 +1,56 @@
 import { json, ActionFunction } from "@remix-run/node";
 import config from "~/config.server";
 import { flattenDepthFirst, getSitemapTree } from "../sitemap";
+import { PassThrough } from "stream";
 
 export const action: ActionFunction = async ({ request }) => {
-  const now = new Date();
-  const sitemapTree = await getSitemapTree();
+  const outputStream = new PassThrough();
 
-  const onlyEditedLastNSeconds = new URL(request.url).searchParams.get(
-    "onlyEditedLastNSeconds"
-  );
-  const seconds =
-    onlyEditedLastNSeconds && Number.parseInt(onlyEditedLastNSeconds);
+  async function inner() {
+    const now = new Date();
+    outputStream.write("🌍 fetching sitemap\n");
+    const sitemapTree = await getSitemapTree();
 
-  await Promise.all(
-    flattenDepthFirst(sitemapTree)
-      // If onlyEditedLastNSeconds is given correctly, only purge sites edited during the given time period
-      .filter((page) => {
-        if (Number.isInteger(seconds) && page.lastmod !== undefined) {
-          const diff = Math.abs(
-            Math.floor(
-              (now.getTime() - new Date(page.lastmod).getTime()) / 1000
-            )
-          );
-          return diff < (seconds as number);
-        }
-        return true;
-      })
-      .flatMap((page) => {
-        return [
-          page.path,
-          `${page.path}?_data=${encodeURIComponent(page.codePath)}`,
-        ];
-      })
-      .map((path) => {
-        console.log(`🔥 purging ${path}`);
-        return fetch(`${config.baseUrl}${path}`, {
-          method: "HEAD",
-          headers: { "Cache-Purge": "1" },
-        });
-      })
-  );
+    const onlyEditedLastNSeconds = new URL(request.url).searchParams.get(
+      "onlyEditedLastNSeconds"
+    );
+    const seconds =
+      onlyEditedLastNSeconds && Number.parseInt(onlyEditedLastNSeconds);
 
-  return json(
-    { purged: true },
-    { status: 200, headers: { "cache-control": "no-store" } }
-  );
+    await Promise.all(
+      flattenDepthFirst(sitemapTree)
+        // If onlyEditedLastNSeconds is given correctly, only purge sites edited during the given time period
+        .filter((page) => {
+          if (Number.isInteger(seconds) && page.lastmod !== undefined) {
+            const diff = Math.abs(
+              Math.floor(
+                (now.getTime() - new Date(page.lastmod).getTime()) / 1000
+              )
+            );
+            return diff < (seconds as number);
+          }
+          return true;
+        })
+        .flatMap((page) => {
+          return [
+            page.path,
+            `${page.path}?_data=${encodeURIComponent(page.codePath)}`,
+          ];
+        })
+        .map((path) => {
+          outputStream.write(`🔥 purging ${path}\n`);
+          return fetch(`${config.baseUrl}${path}`, {
+            method: "HEAD",
+            headers: { "Cache-Purge": "1" },
+          });
+        })
+    );
+    outputStream.write("✅ done");
+    outputStream.end();
+  }
+  inner();
+
+  return new Response(outputStream as any, {
+    headers: { "cache-control": "no-store", "content-type": "plain/text" },
+  });
 };
