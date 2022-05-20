aa75d515b0e599398ce1a4222530589e2d5b557c
Julian Jark <juliuxu@gmail.com>
2022-05-20 23:58:49 +0200

    implement streaming cache purge
    also figured out why cache purge didn't seem to work.
    it's because notion time stamps don't include seconds!

---
+++ app/routes/test/cache-purge-watcher-stream.ts
@@ -0,0 +1,54 @@
+import { LoaderFunction } from "@remix-run/node";
+import config from "~/config.server";
+import { flattenDepthFirst, getSitemapTree } from "../sitemap";
+
+// Notion date stamps are by the minute
+// Only other way to purge then is by checking changes in other properties
+const WATCH_INTERVAL = 61 * 1000;
+
+export const loader: LoaderFunction = ({ request }) => {
+  const stream = new ReadableStream({
+    async start(controller) {
+      let before = new Date();
+      before.setMilliseconds(before.getMilliseconds() - WATCH_INTERVAL);
+      while (!request.signal.aborted) {
+        // Fetch sitemap
+        const now = new Date();
+        const pages = flattenDepthFirst(await getSitemapTree());
+
+        // Get changed pages
+        const changedPages = pages.filter(
+          (page) => new Date(page.lastmod) > before
+        );
+
+        // Log
+        changedPages.forEach((page) => {
+          controller.enqueue(`updating ${page.path} ${page.lastmod} \n`);
+        });
+
+        // Duplicate for _data= paths (used when user is navigating)
+        const finalPaths = changedPages.flatMap((page) => [
+          page.path,
+          `${page.path}?_data=${encodeURIComponent(page.codePath)}`,
+        ]);
+
+        // Purge them
+        const requests = finalPaths.map((path) =>
+          fetch(`${config.baseUrl}${path}`, {
+            headers: { "Cache-Purge": "1" },
+          })
+        );
+        await Promise.allSettled(requests);
+
+        // Sleep till next watch
+        before = now;
+        await new Promise((r) => setTimeout(r, WATCH_INTERVAL));
+      }
+      controller.close();
+    },
+  });
+
+  return new Response(stream, {
+    headers: { "cache-control": "no-store", "content-type": "plain/text" },
+  });
+};
