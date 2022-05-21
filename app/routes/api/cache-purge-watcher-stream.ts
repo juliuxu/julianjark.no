4498ca512250445e8f21ef1a61726efe43deff71
Julian Jark <juliuxu@gmail.com>
2022-05-21 18:22:17 +0200

    refactor

---
+++ app/routes/api/cache-purge-watcher-stream.ts
@@ -0,0 +1,37 @@
+import { LoaderFunction } from "@remix-run/node";
+import { getNumberOrUndefined, getOneOfOrUndefined } from "~/common";
+import { createReadableStreamLogger, purgeUpdatedPages } from "./cache-purge";
+
+const DEFAULT_WATCH_INTERVAL = 20 * 1000;
+export const loader: LoaderFunction = ({ request }) => {
+  const url = new URL(request.url);
+  const currentLoglevel =
+    getOneOfOrUndefined(
+      ["silent", "info", "verbose"],
+      url.searchParams.get("loglevel")
+    ) ?? "verbose";
+  const watchInterval =
+    getNumberOrUndefined(url.searchParams.get("watchInterval")) ??
+    DEFAULT_WATCH_INTERVAL;
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const logger = createReadableStreamLogger(controller, currentLoglevel);
+
+      let before = new Date();
+      before.setMilliseconds(before.getMilliseconds() - watchInterval);
+      while (!request.signal.aborted) {
+        const now = new Date();
+        const purgedPages = await purgeUpdatedPages(before, logger);
+        before = now;
+        await new Promise((r) => setTimeout(r, watchInterval));
+        (currentLoglevel || purgedPages.length > 0) && controller.enqueue("\n");
+      }
+      controller.close();
+    },
+  });
+
+  return new Response(stream, {
+    headers: { "cache-control": "no-store", "content-type": "plain/text" },
+  });
+};
--- app/routes/api/cache-purge.ts
+++ app/routes/api/cache-purge.ts
@@ -1,71 +1,132 @@
 import { ActionFunction } from "@remix-run/node";
 import config from "~/config.server";
-import { flattenDepthFirst, getSitemapTree } from "../sitemap";
+import { flattenDepthFirst, getSitemapTree, Page } from "../sitemap";
 import { PassThrough } from "stream";
 import { getDateOrUndefined, getNumberOrUndefined } from "~/common";
+
+export const isChangedPage = (before: Date) => (page: Page) => {
+  // Notion timestamps are stored only to minute precision
+  // This means that if a before timestamp is recorded at 14:25:30
+  // and a changed happens 10 seconds later, at 14:25:40
+  // the change will be recorded as 14:25:00
+  // which is less than 14:25:30. Making comparsions using seconds miss the change
+
+  // One way to mitigate this is to treat any change in the same minute as changed
+  // This will cause more unnecessary updates, but all updates will be caught.
+  // With a watch interval of 20 seconds, we will get at most 2 unnecessary purges
+  const minuteDate = new Date(before);
+  minuteDate.setMilliseconds(0);
+  minuteDate.setSeconds(0);
+
+  return new Date(page.lastmod) >= minuteDate;
+};
+
+export const purgePage = (page: Page) => {
+  const paths = [
+    page.path,
+    `${page.path}?_data=${encodeURIComponent(page.codePath)}`,
+  ];
+  const requests = paths.map((path) =>
+    fetch(`${config.baseUrl}${path}`, {
+      headers: { "Cache-Purge": "1" },
+    })
+  );
+  return Promise.allSettled(requests);
+};
+
+export const purgeUpdatedPages = async (
+  before: Date,
+  logger: (message: string, level: "silent" | "info" | "verbose") => void
+) => {
+  // Fetch sitemap
+  logger("🌍 fetching sitemap", "info");
+  const pages = flattenDepthFirst(await getSitemapTree());
+
+  // Verbose log sorted timestamps
+  logger(`b ${before.toISOString()}`, "verbose");
+  pages
+    .slice()
+    .sort(
+      (a, b) => new Date(b.lastmod).valueOf() - new Date(a.lastmod).valueOf()
+    )
+    .forEach((page) => {
+      logger(`: ${page.lastmod} ${page.path}`, "verbose");
+    });
+
+  // Get changed pages
+  const changedPages = pages.filter(isChangedPage(before));
+
+  // Log which pages are updating
+  changedPages.forEach((page) => {
+    logger(`🔥 updating ${page.path} (${page.title})`, "silent");
+  });
+  if (changedPages.length === 0) {
+    logger("🆗 no pages updated", "info");
+  }
+
+  // Purge the pages
+  await Promise.allSettled(changedPages.flatMap(purgePage));
+  changedPages.length > 0 && logger("✅ done", "info");
+
+  return changedPages;
+};
+
+export const createReadableStreamLogger = (
+  controller: ReadableStreamController<any>,
+  currentLoglevel: "silent" | "info" | "verbose"
+) => {
+  const loglevelStringToInt: Record<typeof currentLoglevel, number> = {
+    silent: 0,
+    info: 1,
+    verbose: 2,
+  };
+  return function readableStreamLogger(
+    message: string,
+    loglevel: typeof currentLoglevel
+  ) {
+    if (loglevelStringToInt[currentLoglevel] >= loglevelStringToInt[loglevel]) {
+      controller.enqueue(message);
+      if (!message.endsWith("\n")) controller.enqueue("\n");
+    }
+  };
+};
 
 export const action: ActionFunction = async ({ request }) => {
   const outputStream = new PassThrough();
 
   const url = new URL(request.url);
-  const searchParams = {
-    onlyEditedLastNSeconds: getNumberOrUndefined(
-      url.searchParams.get("onlyEditedLastNSeconds")
-    ),
-    onlyEditedSinceDate: getDateOrUndefined(
-      url.searchParams.get("onlyEditedSinceDate")
-    ),
+  const onlyEditedLastNSeconds = getNumberOrUndefined(
+    url.searchParams.get("onlyEditedLastNSeconds")
+  );
+  const onlyEditedSinceDate = getDateOrUndefined(
+    url.searchParams.get("onlyEditedSinceDate")
+  );
+
+  const before = () => {
+    if (onlyEditedSinceDate) {
+      return onlyEditedSinceDate;
+    }
+    if (onlyEditedLastNSeconds) {
+      const date = new Date();
+      date.setSeconds(-onlyEditedLastNSeconds);
+      return date;
+    }
+
+    // Everything, i.e. 1970-01-01T00:00:00.000Z
+    return new Date(0);
   };
 
-  async function inner() {
-    const now = new Date();
-    outputStream.write("🌍 fetching sitemap\n");
-    const sitemapTree = await getSitemapTree();
+  const stream = new ReadableStream({
+    async start(controller) {
+      await purgeUpdatedPages(
+        before(),
+        createReadableStreamLogger(controller, "info")
+      );
+      controller.close();
+    },
+  });
 
-    const results = await Promise.all(
-      flattenDepthFirst(sitemapTree)
-        .filter((page) => {
-          if (page.lastmod === undefined) return true;
-
-          if (searchParams.onlyEditedSinceDate) {
-            return new Date(page.lastmod) > searchParams.onlyEditedSinceDate;
-          }
-
-          if (searchParams.onlyEditedLastNSeconds) {
-            const timeSincePageWasLastEdited = Math.abs(
-              Math.floor(
-                (now.getTime() - new Date(page.lastmod).getTime()) / 1000
-              )
-            );
-            return (
-              timeSincePageWasLastEdited < searchParams.onlyEditedLastNSeconds
-            );
-          }
-          return true;
-        })
-        .flatMap((page) => {
-          return [
-            page.path,
-            `${page.path}?_data=${encodeURIComponent(page.codePath)}`,
-          ];
-        })
-        .map((path) => {
-          outputStream.write(`🔥 purging ${path}\n`);
-          return fetch(`${config.baseUrl}${path}`, {
-            method: "HEAD",
-            headers: { "Cache-Purge": "1" },
-          });
-        })
-    );
-    if (results.length === 0) {
-      outputStream.write("🆗 no pages to be purged with given filter\n");
-    }
-    outputStream.write("✅ done\n");
-    outputStream.end();
-  }
-  inner();
-
-  return new Response(outputStream as any, {
+  return new Response(stream, {
     headers: { "cache-control": "no-store", "content-type": "plain/text" },
   });
 };
