301c662a3e597e9031b3ef89efaeeada5ecb83ad
Julian Jark <juliuxu@gmail.com>
2022-05-31 22:00:36 +0200

    title

--- app/routes/drinker.tsx
+++ app/routes/drinker.tsx
@@ -23,23 +23,26 @@ export default function Drinker() {
   const data = useLoaderData<Data>();
   return (
     <div>
-      <div className="h-72 w-full bg-green-100">
-        <nav className="max-w-lg lg:max-w-4xl mx-auto flex flex-wrap gap-x-10 gap-y-7">
-          {data.drinker.map((drink) => (
-            <NavLink
-              key={drink.id}
-              to={slugify(getTitle(drink))}
-              prefetch="intent"
-              className={({ isActive }) =>
-                `p-2 border border-gray-500 bg-white hover:bg-green-600 hover:text-white ${
-                  isActive ? "bg-green-600 text-white" : ""
-                }`
-              }
-            >
-              {getTitle(drink)}
-            </NavLink>
-          ))}
-        </nav>
+      <div className="min-h-72 w-full bg-green-100">
+        <div className="max-w-lg lg:max-w-4xl mx-auto pt-10 pb-16">
+          <h1 className="text-6xl font-semibold">Drinker</h1>
+          <nav className="flex flex-wrap gap-x-10 gap-y-7 mt-8">
+            {data.drinker.map((drink) => (
+              <NavLink
+                key={drink.id}
+                to={slugify(getTitle(drink))}
+                prefetch="intent"
+                className={({ isActive }) =>
+                  `p-2 border border-gray-500 bg-white hover:bg-green-600 hover:text-white ${
+                    isActive ? "bg-green-600 text-white" : ""
+                  }`
+                }
+              >
+                {getTitle(drink)}
+              </NavLink>
+            ))}
+          </nav>
+        </div>
       </div>
       <Outlet />
     </div>
