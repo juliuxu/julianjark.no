2f0eb19571259e6620691ab4fbbf579643e31dbf
Julian Jark <juliuxu@gmail.com>
2022-10-30 19:00:23 +0100

    update

--- app/routes/__layout/index.tsx
+++ app/routes/__layout/index.tsx
@@ -62,7 +62,7 @@ export const meta: MetaFunction = () => {
   const [age] = getAgeFromBirthDate(new Date("1992-11-02"));
   return {
     title: "Julian Jark",
-    description: `${age} år gammel hundeelsker, drinkmaker, turgåer, musikkavspiller, og kodeskriver`,
+    description: `${age} år gammel hundevenn, drinkmaker, turgåer, kodeskriver, musikkavspiller`,
   };
 };
 
