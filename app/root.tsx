995cb18d6c46425e35e470e49e099bafb1e984a4
Julian Jark <juliuxu@gmail.com>
2023-03-18 02:52:56 +0100

    try to track page view with matomo

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import type { LinksFunction } from "@remix-run/node";
 import {
   isRouteErrorResponse,
@@ -7,6 +8,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLocation,
   useRouteError,
 } from "@remix-run/react";
 
@@ -51,25 +53,36 @@ export default function App() {
         />
 
         {/* Matomo */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-        var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="//analytics.julianjark.no/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '1']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();
-        `,
-          }}
-        />
+        <Matomo />
       </body>
     </html>
+  );
+}
+
+function Matomo() {
+  const location = useLocation();
+  useEffect(() => {
+    (window as any)._paq?.push(["trackPageView"]);
+  }, [location]);
+
+  return (
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.julianjark.no/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+  `,
+      }}
+    />
   );
 }
 
