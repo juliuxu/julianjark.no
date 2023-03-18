560c5edd3c6c4dd935929cf0b1395d4f62d4a4a6
Julian Jark <juliuxu@gmail.com>
2023-03-18 05:12:13 +0100

    very well

--- app/root.tsx
+++ app/root.tsx
@@ -83,10 +83,10 @@ function Matomo() {
       <script
         dangerouslySetInnerHTML={{
           __html: `
-          window._paq = window._paq || [];
-          window._paq.push(["enableLinkTracking"]);
-          window._paq.push(["setTrackerUrl", "https://analytics.julianjark.no/popcorn"]);
-          window._paq.push(["setSiteId", "1"]);
+          var _paq = window._paq = window._paq || [];
+          _paq.push(["enableLinkTracking"]);
+          _paq.push(["setTrackerUrl", "https://analytics.julianjark.no/popcorn"]);
+          _paq.push(["setSiteId", "1"]);
           `,
         }}
       />
