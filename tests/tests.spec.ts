f71ac0d6809391302f6115ff79b0923793ab28c3
Julian Jark <juliuxu@gmail.com>
2023-03-23 15:47:54 +0100

    upgrade and add tests

--- tests/tests.spec.ts
+++ tests/tests.spec.ts
@@ -5,15 +5,3 @@ test("landing page", async ({ page }) => {
 
   await expect(page).toHaveTitle(/Julian Jark/);
 });
-
-test("today i learned", async ({ page }) => {
-  await page.goto("http://localhost:3000/today-i-learned");
-
-  await expect(page).toHaveTitle(/I dag lærte jeg/);
-
-  const articles = page.getByRole("article");
-  await expect(articles).not.toHaveCount(0);
-  await expect(
-    articles.first().getByRole("heading", { level: 2 }),
-  ).toBeVisible();
-});
---
+++ tests/today-i-learned.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from "@playwright/test";
+
+test("today i learned", async ({ page }) => {
+  await page.goto("http://localhost:3000/today-i-learned");
+
+  await expect(page).toHaveTitle(/I dag lærte jeg/);
+
+  const articles = page.getByRole("article");
+  await expect(articles).not.toHaveCount(0);
+
+  // a single entry
+  const article = articles.first();
+  await expect(article.getByRole("heading", { level: 2 })).toBeVisible();
+
+  const articleTitle = await article
+    .getByRole("heading", { level: 2 })
+    .innerText();
+
+  await article.click();
+  await expect(page).toHaveTitle(new RegExp(articleTitle));
+});
